2fdc3aa5f64b3e9e105dd98531b00f4bf72797d0
Belén Albeza <belen.albeza@gmail.com>
2016-04-16 14:55:51 +0200

    scale & enable crisp rendering

--- src/js/main.js
+++ src/js/main.js
@@ -4,39 +4,48 @@ var PlayScene = require('./play_scene.js');
 
 
 var BootScene = {
-  preload: function () {
-    // load here assets required for the loading screen
-    this.game.load.image('preloader_bar', 'images/preloader_bar.png');
-  },
+    init: function () {
+        // 3x scale
+        this.game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
+        this.game.scale.setUserScale(3, 3);
+        // enable crisp rendering
+        this.game.renderer.renderSession.roundPixels = true;
+        Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
+    },
 
-  create: function () {
-    this.game.state.start('preloader');
-  }
+    preload: function () {
+        // load here assets required for the loading screen
+        this.game.load.image('preloader_bar', 'images/preloader_bar.png');
+    },
+
+    create: function () {
+        this.game.state.start('preloader');
+    }
 };
 
 
 var PreloaderScene = {
-  preload: function () {
-    this.loadingBar = this.game.add.sprite(0, 240, 'preloader_bar');
-    this.loadingBar.anchor.setTo(0, 0.5);
-    this.load.setPreloadSprite(this.loadingBar);
+    preload: function () {
+        this.loadingBar = this.game.add.sprite(0, 240, 'preloader_bar');
+        this.loadingBar.anchor.setTo(0, 0.5);
+        this.load.setPreloadSprite(this.loadingBar);
 
-    // TODO: load here the assets for the game
-    this.game.load.image('logo', 'images/phaser.png');
-  },
+        // TODO: load here the assets for the game
+        this.game.load.image('logo', 'images/phaser.png');
+    },
 
-  create: function () {
-    this.game.state.start('play');
-  }
+    create: function () {
+        this.game.state.start('play');
+    }
 };
 
 
 window.onload = function () {
-  var game = new Phaser.Game(320, 200, Phaser.AUTO, 'game');
+    var game = new Phaser.Game(320, 200, Phaser.AUTO, 'game');
 
-  game.state.add('boot', BootScene);
-  game.state.add('preloader', PreloaderScene);
-  game.state.add('play', PlayScene);
+    game.state.add('boot', BootScene);
+    game.state.add('preloader', PreloaderScene);
+    game.state.add('play', PlayScene);
 
-  game.state.start('boot');
+    game.state.start('boot');
 };
